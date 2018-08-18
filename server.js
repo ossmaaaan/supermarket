740ce9b8521267dd2727854942d4991277d8d8ab
red elmo <ridwan.taza@gmail.com>
2018-08-18 21:46:46 +0200

    add a server file & builded files

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const api = require('./server/routes/api');
+
+const port = process.env.PORT || 8080;
+
+app = express();
+
+app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'uploads')));
+app.use(express.static(path.join(__dirname, 'uploads/screenshoots')));
+
+
+app.use(bodyParser.urlencoded({extends: true}));
+app.use(bodyParser.json());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.use('/api', api);
+
+app.get('*', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+})
+
+app.listen(port);
